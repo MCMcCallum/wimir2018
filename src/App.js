572bf61dd9fd2056fcac2ea821b7d4900a5fca69
Matthew McCallum <matt.c.mccallum@gmail.com>
2018-12-15 12:57:47 -0800

    * Added bare minimum Magenta drum beat code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as mm from '@magenta/music';
 
 class App extends Component {
-  render() {
+///
+/// This class encapsulates a drum pattern generator.
+///
+
+  constructor(props) 
+  ///
+  /// Here we initialize state variables and load the model.
+  ///
+  {
+    super(props);
+
+    // Bind methods.
+    this.handleNewDrums = this.handleNewDrums.bind(this);
+
+    // Initisalize state.
+    this.state = {
+      loading: true
+    };
+
+    // Create drum pattern generator
+    this.config_beat = {
+      noteHeight: 6,
+      pixelsPerTimeStep: 20,  // like a note width
+      noteSpacing: 1,
+      noteRGB: '8, 41, 64',
+      activeNoteRGB: '240, 84, 119',
+    };
+    this.vae_model = new mm.MusicVAE('https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/drums_2bar_nade_9_q2');
+    this.vae_model.initialize()
+      .then(() => this.setState({loading: false}))
+      .then(() => this.handleNewDrums());
+  }
+
+  handleNewDrums() 
+  ///
+  /// This function is to be called every time we get a new
+  /// drum pattern. It creates a new visualizer and loads the
+  /// drum samples into it.
+  ///
+  {
+    return new Promise((resolve, reject) => {
+      this.vae_model.sample(1)
+        .then((samples) => {
+        var drum_samples = samples[0];
+        var viz_vae = new mm.Visualizer(drum_samples, document.getElementById('vaecanvas'), this.config_beat);
+        })
+        .then(() => resolve());
+    });
+  }
+
+  render() 
+  ///
+  /// Render the HTML. This provides HTML content, based
+  /// on the state in this JavaScript object, to render
+  /// in a web browser.
+  ///
+  {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div hidden={!this.state.loading}>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            Loading...
+          </header>
+        </div>
+        <div hidden={this.state.loading}>
+          <div className="Beat">
+            <canvas id="vaecanvas"></canvas>
+          </div>
+        </div>
       </div>
     );
   }
