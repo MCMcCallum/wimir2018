3c4d2d791f34057a8cef7d32e545cc901f5c42c0
Matthew McCallum <matt.c.mccallum@gmail.com>
2018-12-16 14:35:42 -0800

    * Added playback controls

--- src/App.js
+++ src/App.js
@@ -17,10 +17,16 @@ class App extends Component {
 
     // Bind methods.
     this.handleNewDrums = this.handleNewDrums.bind(this);
+    this.handlePlay = this.handlePlay.bind(this);
+    this.handleStop = this.handleStop.bind(this);
+    this.stopped = this.stopped.bind(this);
+    this.continuePlaying = this.continuePlaying.bind(this);
 
     // Initisalize state.
     this.state = {
-      loading: true
+      loading: true,
+      playing: false,
+      stopping: false
     };
 
     // Create drum pattern generator
@@ -37,6 +43,14 @@ class App extends Component {
       .then(() => this.handleNewDrums());
   }
 
+  continuePlaying()
+  ///
+  /// Play one iteration of the drum loop.
+  ///
+  {
+    this.viz_player_vae.start(this.drum_samples, 120.0);
+  }
+
   handleNewDrums() 
   ///
   /// This function is to be called every time we want a new
@@ -47,10 +61,54 @@ class App extends Component {
     return new Promise((resolve, reject) => {
       this.vae_model.sample(1)
         .then((samples) => {
-        var drum_samples = samples[0];
-        var viz_vae = new mm.Visualizer(drum_samples, document.getElementById('vaecanvas'), this.config_beat);
+          this.drum_samples = samples[0];
+          var viz_vae = new mm.Visualizer(this.drum_samples, document.getElementById('vaecanvas'), this.config_beat);
+          this.viz_player_vae = new mm.Player(false, {
+            run: (note) => viz_vae.redraw(note),
+            stop: () => {
+              if(!this.state.stopping){
+                this.continuePlaying();
+              } else {
+                this.stopped();
+              }
+            }
+          });
         })
         .then(() => resolve());
+    });
+  }
+
+  handlePlay() 
+  ///
+  /// Start playback.
+  ///
+  {
+    this.setState({
+      playing: true,
+      stopping: false
+    });
+    this.continuePlaying();
+  }
+
+  handleStop()
+  ///
+  /// Stop playback.
+  ///
+  {
+    if(this.state.playing)
+    {
+      this.setState({stopping: true})
+    }
+  }
+
+  stopped()
+  ///
+  /// Set state when stopping is complete.
+  ///
+  {
+    this.setState({
+      playing: false,
+      stopping: false
     });
   }
 
@@ -70,10 +128,14 @@ class App extends Component {
           </header>
         </div>
         <div hidden={this.state.loading}>
+          <div className="PlayControls">
+            <button className="transport_controls" onClick={this.handlePlay} disabled={this.state.playing} data-toggle="button"><b>Play Loop</b></button>
+            <button className="transport_controls" onClick={this.handleStop} disabled={!this.state.playing} data-toggle="button"><b>{this.state.stopping ? "Stopping..." : "Stop Loop"}</b></button>
+          </div>
           <div className="Beat">
             <canvas id="vaecanvas"></canvas>
             <br/>
-            <button className="control" onClick={this.handleNewDrums} data-toggle="button"><b>Shuffle Beat</b></button>
+            <button className="control" onClick={this.handleNewDrums} disabled={this.state.recording || this.state.playing} data-toggle="button"><b>Shuffle Beat</b></button>
           </div>
         </div>
       </div>
